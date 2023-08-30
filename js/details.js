ec2a8b19897f9c24cf17caa21377c7e2b342c814
Jesus <janeirojm91@gmail.com>
2023-08-29 21:36:44 -0600

    se agregan comentarios a la linea del codigo

--- js/details.js
+++ js/details.js
@@ -1,72 +1,72 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const params = new URLSearchParams(window.location.search);
-  const movieIndex = parseInt(params.get("movieIndex"));
+document.addEventListener("DOMContentLoaded", () => { //se carga el DOM de la pagina y se ejecuta la funcion de hacer fetch del json
+  const params = new URLSearchParams(window.location.search); 
+  const movieIndex = parseInt(params.get("movieIndex")); 
 
-  fetch("js/db.json")
+  fetch("js/db.json") 
     .then((res) => res.json())
     .then((data) => {
       const movie = data[movieIndex];
 
-      const container = document.querySelector(".container");
+      const container = document.querySelector(".container"); // se crea una constante que selecciona el container del HTML
 
-      const movieCard = document.createElement("div");
-      movieCard.classList.add("movieCard");
+      const movieCard = document.createElement("div"); //se crea una constante y se usa un createElement para crear un div
+      movieCard.classList.add("movieCard"); //se le asigna la clase movieCard al div creado previamente
 
-      const movieTitle = document.createElement("h2");
-      movieTitle.classList.add("movieTitle");
-      movieTitle.textContent = movie.title;
+      const movieTitle = document.createElement("h2"); //se crea una constante y se usa un createElement para crear una etiqueta de h2
+      movieTitle.classList.add("movieTitle"); //se crea una constante y se usa un createElement para crear una etiqueta de h2
 
-      const movieRank = document.createElement("p");
-      movieRank.classList.add("movieRank");
-      movieRank.textContent = `Ranking: ${movie.rank}`;
+      const movieRank = document.createElement("p"); //se crea una constante y se usa un createElement para crear una etiqueta de párrafo
+      movieRank.classList.add("movieRank"); //se le asigna la clase movieRank al p creado previamente
 
-      const movieDescription = document.createElement("p");
-      movieDescription.classList.add("movieDescription");
-      movieDescription.textContent = movie.description;
+      const movieDescription = document.createElement("p"); //se crea una constante y se usa un createElement para crear una etiqueta de párrafo
+      movieDescription.classList.add("movieDescription"); //se le asigna la clase movieDescription al p creado previamente
 
-      const movieGenre = document.createElement("p");
-      movieGenre.classList.add("movieGenre");
-      movieGenre.textContent = `Genre: ${movie.genre.join(", ")}`;
+      const movieGenre = document.createElement("p"); //se crea una constante y se usa un createElement para crear una etiqueta de párrafo
+      movieGenre.classList.add("movieGenre"); //se le asigna la clase movieGenre al p creado previamente
 
-      const movieRating = document.createElement("p");
-      movieRating.classList.add("movieRating");
-      movieRating.textContent = `Rating: ${movie.rating}`;
+      const movieRating = document.createElement("p"); //se crea una constante y se usa un createElement para crear una etiqueta de párrafo
+      movieRating.classList.add("movieRating"); //se le asigna la clase movieRating al p creado previamente
 
-      const movieYear = document.createElement("p");
-      movieYear.classList.add("movieYear");
-      movieYear.textContent = `Year: ${movie.year}`;
+      const movieYear = document.createElement("p"); //se crea una constante y se usa un createElement para crear una etiqueta de párrafo
+      movieYear.classList.add("movieYear"); //se le asigna la clase movieYear al p creado previamente
 
-      const movieImage = document.createElement("img");
-      movieImage.classList.add("imgMovie");
-      movieImage.src = movie.image;
+      const movieImage = document.createElement("img"); //se crea una constante y se usa un createElement para crear una etiqueta de imagen
+      movieImage.classList.add("imgMovie"); //se le asigna la clase imgMovie al img creado previamente
 
-      container.appendChild(movieCard);
-      movieCard.appendChild(movieTitle);
-      movieCard.appendChild(movieRank);
-      movieCard.appendChild(movieDescription);
-      movieCard.appendChild(movieGenre);
-      movieCard.appendChild(movieRating);
-      movieCard.appendChild(movieYear);
-      movieCard.appendChild(movieImage);
+      container.appendChild(movieCard); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      movieCard.appendChild(movieTitle); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      movieCard.appendChild(movieRank); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      movieCard.appendChild(movieDescription); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      movieCard.appendChild(movieGenre); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      movieCard.appendChild(movieRating); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      movieCard.appendChild(movieYear); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      movieCard.appendChild(movieImage); //con el appendChild se agregan nuevos elementos a nuestro HTML
 
-      const videoContainer = document.createElement("div");
-      videoContainer.classList.add("videoContainer");
+      const videoContainer = document.createElement("div"); //se crea una constante y se usa un createElement para crear un div
+      videoContainer.classList.add("videoContainer"); //se le asigna la clase movieCard al div creado previamente
 
-      const movieYouTubeIframe = document.createElement("iframe");
-      movieYouTubeIframe.setAttribute("src", movie.youtube); 
-      movieYouTubeIframe.setAttribute("width", "600");
-      movieYouTubeIframe.setAttribute("height", "350");
-      movieYouTubeIframe.setAttribute("frameborder", "0");
-      movieYouTubeIframe.setAttribute("allowfullscreen", "");
+      const movieTrailer = document.createElement("h2"); //se crea una constante y se usa un createElement para crear una etiqueta de h2
+      movieTrailer.classList.add("movieTrailer"); //se crea una constante y se usa un createElement para crear una etiqueta de h2
 
-      const movieTrailer = document.createElement("h2");
-      movieTrailer.classList.add("movieTrailer");
+      movieTitle.textContent = movie.title; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+      movieRank.textContent = `Ranking: ${movie.rank}`; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+      movieDescription.textContent = movie.description; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+      movieGenre.textContent = `Genre: ${movie.genre.join(", ")}`; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+      movieRating.textContent = `Rating: ${movie.rating}`; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+      movieYear.textContent = `Year: ${movie.year}`; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+      movieImage.src = movie.image; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
       movieTrailer.textContent = `${movie.title} Trailer`;
 
-      container.appendChild(videoContainer);
-      videoContainer.appendChild(movieTrailer); // Move h2 into the videoContainer
-      videoContainer.appendChild(movieYouTubeIframe);
-      
+      const movieYouTubeIframe = document.createElement("iframe"); // creamos una etiqueta de iframe para indexarla al HTML
+      movieYouTubeIframe.setAttribute("src", movie.youtube); // propiedades del iframe
+      movieYouTubeIframe.setAttribute("width", "600"); // propiedades del iframe
+      movieYouTubeIframe.setAttribute("height", "350"); // propiedades del iframe
+      movieYouTubeIframe.setAttribute("frameborder", "0"); // propiedades del iframe
+      movieYouTubeIframe.setAttribute("allowfullscreen", ""); // propiedades del iframe
+
+      container.appendChild(videoContainer); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      videoContainer.appendChild(movieTrailer); //con el appendChild se agregan nuevos elementos a nuestro HTML
+      videoContainer.appendChild(movieYouTubeIframe); //con el appendChild se agregan nuevos elementos a nuestro HTML
     })
     .catch((error) => console.error("Error fetching data:", error));
 });
--- js/script.js
+++ js/script.js
@@ -1,64 +1,64 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { //se carga el DOM de la pagina y se ejecuta la funcion de hacer fetch del json
   fetch("js/db.json")
     .then((res) => res.json())
     .then((data) => {
-      const movieData = data.slice(0, 50);
+      const movieData = data.slice(0, 50); //se crea una constante que parte el data de la db en 50 elementos
 
-      const container = document.querySelector(".container");
+      const container = document.querySelector(".container"); // se crea una constante que selecciona el container del HTML
 
-      movieData.forEach((movie, index) => {
-        const movieCard = document.createElement("div");
-        movieCard.classList.add("movieCard");
+      movieData.forEach((movie, index) => { // se usa un foreach para hacer un callback desde el index y crear elementos
+        const movieCard = document.createElement("div"); //se crea una constante y se usa un createElement para crear un div
+        movieCard.classList.add("movieCard"); //se le asigna la clase movieCard al div creado previamente
 
-        const movieTitle = document.createElement("h2");
-        movieTitle.classList.add("movieTitle");
+        const movieTitle = document.createElement("h2"); //se crea una constante y se usa un createElement para crear una etiqueta de h2
+        movieTitle.classList.add("movieTitle"); //se le asigna la clase movieTitle al h2 creado previamente
 
-        const movieRank = document.createElement("p");
-        movieRank.classList.add("movieRank");
+        const movieRank = document.createElement("p"); //se crea una constante y se usa un createElement para crear una etiqueta de párrafo
+        movieRank.classList.add("movieRank"); //se le asigna la clase movieRank al p creado previamente
 
-        const movieGenre = document.createElement("p");
-        movieGenre.classList.add("movieGenre");
+        const movieGenre = document.createElement("p"); //se crea una constante y se usa un createElement para crear una etiqueta de párrafo
+        movieGenre.classList.add("movieGenre"); //se le asigna la clase movieGenre al p creado previamente
 
-        const movieImage = document.createElement("img");
-        movieImage.classList.add("imgMovie");
+        const movieImage = document.createElement("img");  //se crea una constante y se usa un createElement para crear una etiqueta de imagen
+        movieImage.classList.add("imgMovie"); //se le asigna la clase imgMovie al img creado previamente
 
-        container.appendChild(movieCard);
-        movieCard.appendChild(movieTitle);
-        movieCard.appendChild(movieRank);
-        movieCard.appendChild(movieGenre);
-        movieCard.appendChild(movieImage);
+        container.appendChild(movieCard); //con el appendChild se agregan nuevos elementos a nuestro HTML
+        movieCard.appendChild(movieTitle); //con el appendChild se agregan nuevos elementos a nuestro HTML
+        movieCard.appendChild(movieRank); //con el appendChild se agregan nuevos elementos a nuestro HTML
+        movieCard.appendChild(movieGenre); //con el appendChild se agregan nuevos elementos a nuestro HTML 
+        movieCard.appendChild(movieImage); //con el appendChild se agregan nuevos elementos a nuestro HTML 
 
-        movieTitle.textContent = movie.title;
-        movieRank.textContent = "Ranking: " + movie.rank;
-        movieGenre.textContent = "Genre: " + movie.genre.join(", ");
-        movieImage.src = movie.image;
-        movieImage.alt = `${movie.title} Image`;
+        movieTitle.textContent = movie.title; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+        movieRank.textContent = "Ranking: " + movie.rank; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+        movieGenre.textContent = "Genre: " + movie.genre.join(", "); //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+        movieImage.src = movie.image; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
+        movieImage.alt = `${movie.title} Image`; //se accede al DOM y se modifican las propiedades de las etiquetas llamando al elemento del json
 
-        movieCard.addEventListener("click", () => {
-          const queryParams = new URLSearchParams();
-          queryParams.append("movieIndex", index);
+        movieCard.addEventListener("click", () => { // se crea un listener que al hacer click almacene los parametros del contenido de la pagina 
+          const queryParams = new URLSearchParams(); 
+          queryParams.append("movieIndex", index); 
 
-          const queryString = queryParams.toString();
-          const detailsPageURL = `details.html?${queryString}`;
+          const queryString = queryParams.toString(); // convierte los parametros obtenidos en string
+          const detailsPageURL = `details.html?${queryString}`; //se crea la URL que manejara la info de los detalles de cada pelicula
 
-          window.open(detailsPageURL);
+          window.open(detailsPageURL); // abre una pestaña con la información de la película
         });
       });
 
-      const finderInput = document.querySelector(".finder");
+      const finderInput = document.querySelector(".finder"); // se selecciona el input de busqueda
 
-      finderInput.addEventListener("input", () => {
-        const filtro = finderInput.value.toLowerCase();
+      finderInput.addEventListener("input", () => { //se crea un listener del input que tomara el valor escrito dentro del input
+        const filtro = finderInput.value.toLowerCase(); //convierte cualquier valor del input en minusuculas, para no generar errores posteriormente o que no filtre correctamente
 
-        movieData.forEach((movie, index) => {
-          const movieCard = document.querySelectorAll(".movieCard")[index];
+        movieData.forEach((movie, index) => { //con un forEach hara un callback de la db para filtrar desde el index la info que haya capturado el input
+          const movieCard = document.querySelectorAll(".movieCard")[index]; 
           const titleMatch = movie.title.toLowerCase().includes(filtro);
           const genreMatch = movie.genre.some(genre => genre.toLowerCase().includes(filtro));
 
-          if (titleMatch || genreMatch) {
-            movieCard.style.display = "block";
+          if (titleMatch || genreMatch) { // con el condicional if filtrara el titleMatch O el genero para mostrar coincidencias.
+            movieCard.style.display = "block"; // si hay coincidencias las ordenara por medio de bloque
           } else {
-            movieCard.style.display = "none";
+            movieCard.style.display = "none"; // si no hay coincidencias no mostrara nada
           }
         });
       });
